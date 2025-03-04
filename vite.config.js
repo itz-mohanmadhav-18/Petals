455e46218421d8764997b8f85ed7a1352c0f19f4
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-03-04 13:17:15 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Assuming this exists in v4+
 
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss() // Using the official plugin
